f1a6875716b02f98ca5ff17f94fe5f2af7e6c439
LakshmishaGopalchar <58968895+LakshmishaGopalchar@users.noreply.github.com>
2020-06-16 16:14:07 +0400

    Update application.py

--- application.py
+++ application.py
@@ -7,7 +7,7 @@ from flask import Flask, jsonify
 from flask import request
 
 
-sql_conn = pyodbc.connect('DRIVER={ODBC Driver 17 for SQL Server};SERVER=MAFVDCBISQL03;DATABASE=MAFVCUSTDW;Trusted_Connection=yes') 
+sql_conn = pyodbc.connect('DRIVER={ODBC Driver 13 for SQL Server};SERVER=MAFVDCBISQL03;DATABASE=MAFVCUSTDW;Trusted_Connection=yes') 
 
 query="select B.DataAreaID as company,B.GlobalNumber,B.TransDate,D.[Item ID],D.[Subgroup Code],D.Division,D.[Category Name],D.Subgroup,Sum(B.NetAmountAED) as NetAmount,Sum(B.Qty) as Qty from FactFashionTransLine B join DimFashionStores C on B.Store = C.StoreNumber join (select DimFashionRetailProduct.Company,DimFashionRetailProduct.[Item ID],DimFashionRetailProduct.Division,DimFashionRetailProduct.[Category Name],DimFashionRetailProduct.[Subgroup Code],DimFashionRetailProduct.Subgroup from DimFashionRetailProduct group by DimFashionRetailProduct.Company, DimFashionRetailProduct.[Item ID], DimFashionRetailProduct.Division, DimFashionRetailProduct.[Category Name], DimFashionRetailProduct.[Subgroup Code], DimFashionRetailProduct.Subgroup) as D on B.ItemID = D.[Item ID] and B.DataAreaID = D.Company left join stgFashionExclusionList as E on B.GlobalNumber = E.GlobalNumber where B.GlobalNumber is not NULL and C.StoreBrandCode = 'CnB' and B.CustAccount not Like 'R%' and B.NetAmountAED > 0 and E.GlobalNumber is NULL group by B.DataAreaID, B.GlobalNumber, B.TransDate, D.[Item ID], D.[Subgroup Code], D.Division, D.[Category Name], D.Subgroup"
 
