1f74c21d66029615d385f66b2f33bcc931a6d878
LakshmishaGopalchar <58968895+LakshmishaGopalchar@users.noreply.github.com>
2020-06-17 16:42:46 +0400

    Update application.py

--- application.py
+++ application.py
@@ -6,18 +6,18 @@ from flask import Flask, jsonify
 #import spacy
 from flask import request
 
-server = 'mafvdata.database.windows.net'
-database = 'mafvaa_in'
-username = 'aateam'
-password = 'uKPdyRRNEK7qQ9xS'
+server = 'MAFVAZEBISQL01'
+database = 'TX_MAFFashionDW'
+#username = 'aateam'
+#password = 'uKPdyRRNEK7qQ9xS'
 #driver= '{ODBC Driver 17 for SQL Server}'
 drivers = [item for item in pyodbc.drivers()]
 driver = drivers[-1]
-cnxn = pyodbc.connect('DRIVER='+driver+';PORT=1433;SERVER='+server+';PORT=1443;DATABASE='+database+';UID='+username+';PWD='+ password)
+cnxn = pyodbc.connect('DRIVER='+driver+';SERVER='+server+';DATABASE='+database+';'Trusted_Connection=yes')
 cursor = cnxn.cursor()
 
 
-cursor.execute("Select * from [VOX].[DimItem_V]")
+cursor.execute("select distinct replace([Item ID],'-LEGO','') ItemID  from TempRetailProduct where [Brand Code] = 'LEGO'")
 row = cursor.fetchone()
 while row:
     print (str(row[0]) + " " + str(row[1]))
